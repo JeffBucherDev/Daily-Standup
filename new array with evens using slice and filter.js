7b1f3b148b38674304cb01d71920c74ee1d3bc3f
Jbucher89 <calmincolorado@gmail.com>
2021-10-15 16:05:01 -0600

    forgot a comma

--- new array with evens using slice and filter.js
+++ new array with evens using slice and filter.js
@@ -7,7 +7,7 @@ For example:
 ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
-P = we ar taking in an array and a number / all arrays will have at least 1 even number / only nuymbers in the array / positive or negative numbers in array, only positive #'s for second argument
+P = we are taking in an array and a number / all arrays will have at least 1 even number / only nuymbers in the array / positive or negative numbers in array, only positive #'s for second argument
 R = returns an array of even numbers / only numbers will be returned/ even nums can be positive or negative / amount of returned numbers in the array will match the 2nd argument of function
 E = console.log(extractEndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8])
     console.log(extractEndEvens([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), [-8, 26])
@@ -23,3 +23,8 @@ P = 1.loop through and remove all even numbers
 function extractEndEvens(arr, numOfEvenNums) {
 return arr.filter(num => num % 2 === 0).slice(-numOfEvenNums)
 }
+
+console.log(extractEndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8])
+console.log(extractEndEvens([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), [-8, 26])
+console.log(extractEndEvens([6, -25, 3, 7, 5, 5, 7, -3, 23], 1), [6])
+console.log(extractEndEvens([4, 2, 3, 21, 13, 32, 26, 1, 7, 3, 15, 22, 4, 8, 2, 1, 5, 9, 12], 6), [26, 22, 4, 8, 2, 12])
